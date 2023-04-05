39b776d4cd3eb5227b49f9063b48be8e73c9a983
royanlord <royanfarid32@gmail.com>
2023-04-05 19:32:18 +0700

    add sticky nav and styling sticky nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// sticky nav
+var nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  if(document.documentElement.scrollTop > 250) {
+    nav.classList.add("sticky")
+  } else {
+    nav.classList.remove("sticky")
+  }
+})
+
 // typed.js
 const typing = new Typed(".passion", {
     strings: ["Front-End Web Developer", "UIUX Designer"],
